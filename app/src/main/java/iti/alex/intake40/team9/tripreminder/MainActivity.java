6370c691eff19d86fb7bf9ac01a53d9462793b88
Mahmoud-Abdelwahab <mahmoudabdelwahab199390@gmail.com>
2020-03-17 10:34:15 +0200

    creating repo

---
+++ app/src/main/java/iti/alex/intake40/team9/tripreminder/MainActivity.java
@@ -0,0 +1,14 @@
+package iti.alex.intake40.team9.tripreminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/iti/alex/intake40/team9/tripreminder/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package iti.alex.intake40.team9.tripreminder;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
