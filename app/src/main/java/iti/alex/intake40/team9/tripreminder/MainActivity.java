0ea467142a364acd72ff3cee6f8cec9fd9c8e51d
mahmoudfathy01010 <58287190+mahmoudfathy01010@users.noreply.github.com>
2020-03-19 22:19:56 +0200

    Update MainActivity.java

--- app/src/main/java/iti/alex/intake40/team9/tripreminder/MainActivity.java
+++ app/src/main/java/iti/alex/intake40/team9/tripreminder/MainActivity.java
@@ -10,11 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
         System.out.println("Hello Git");
         System.out.println("Hello ’ِMahmoud");
-=======
         System.out.println("mahmoud Abdelwahab");
->>>>>>> master
     }
 }
