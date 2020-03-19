a433a3b62bf3b24df7be737b3cfa1c6c4cc4ef1b
mahmoudfathy01010 <58287190+mahmoudfathy01010@users.noreply.github.com>
2020-03-20 00:48:47 +0200

    Create LoginView.java

---
+++ app/src/main/java/iti/alex/intake40/team9/tripreminder/Views/LoginView.java
@@ -0,0 +1,17 @@
+package iti.alex.intake40.team9.tripreminder.Views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import iti.alex.intake40.team9.tripreminder.R;
+
+public class LoginView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
