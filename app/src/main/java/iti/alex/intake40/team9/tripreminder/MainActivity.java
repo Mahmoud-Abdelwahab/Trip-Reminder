b9187a4db07c63a0e1c13b80b8c0a78b9085ab63
Mahmoud-Abdelwahab <mahmoudabdelwahab199390@gmail.com>
2020-03-19 20:51:07 +0200

    Update MainActivity.java
    kacomm
    
    Co-Authored-By: Mahmoud-Abdelwahab <mahmoud-abdelwahab@users.noreply.github.com>

--- app/src/main/java/iti/alex/intake40/team9/tripreminder/MainActivity.java
+++ app/src/main/java/iti/alex/intake40/team9/tripreminder/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+      System.out.println("7amoksha");
+        System.out.println("  kasper was here ! ");
+
     }
 }
