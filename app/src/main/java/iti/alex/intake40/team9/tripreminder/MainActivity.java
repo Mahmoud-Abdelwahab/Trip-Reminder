bae5762955a3b1a99087c090e7307a0af3203fa4
mahmoudfathy01010 <58287190+mahmoudfathy01010@users.noreply.github.com>
2020-03-19 22:09:37 +0200

    Update MainActivity.java

--- app/src/main/java/iti/alex/intake40/team9/tripreminder/MainActivity.java
+++ app/src/main/java/iti/alex/intake40/team9/tripreminder/MainActivity.java
@@ -12,6 +12,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("Hello Git");
         System.out.println("Hello ’ِMahmoud");
-
     }
 }
