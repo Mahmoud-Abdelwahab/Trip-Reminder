cb676177b6a68692dafeaa386be4897fb933dc04
Mahmoud-Abdelwahab <mahmoudabdelwahab199390@gmail.com>
2020-03-19 20:39:05 +0200

    Update MainActivity.java
    test
    
    Co-Authored-By: Mahmoud-Abdelwahab <mahmoud-abdelwahab@users.noreply.github.com>

--- app/src/main/java/iti/alex/intake40/team9/tripreminder/MainActivity.java
+++ app/src/main/java/iti/alex/intake40/team9/tripreminder/MainActivity.java
@@ -10,10 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("mahmoud Abdelwahab");
-
-        System.out.println("mahmoud  kasper branch");
-        System.out.println("mahmoud  kasper branch");
-
     }
 }
