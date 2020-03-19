5d77ececc5e42849e4a70c5581d70c2f918b711d
mahmoudfathy01010 <58287190+mahmoudfathy01010@users.noreply.github.com>
2020-03-20 01:31:35 +0200

    Update NavigationDrawerView.java

--- app/src/main/java/iti/alex/intake40/team9/tripreminder/Views/NavigationDrawerView.java
+++ app/src/main/java/iti/alex/intake40/team9/tripreminder/Views/NavigationDrawerView.java
@@ -1,5 +1,6 @@
 package iti.alex.intake40.team9.tripreminder.Views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -7,10 +8,14 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 import iti.alex.intake40.team9.tripreminder.R;
 
-public class NavigationDrawerView extends AppCompatActivity {
+public class NavigationDrawerView extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawer;
 
@@ -20,9 +25,17 @@ public class NavigationDrawerView extends AppCompatActivity {
        Toolbar toolbar=findViewById(R.id.toolbar);
        setSupportActionBar(toolbar);
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView=findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        if(savedInstanceState==null)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new UpcomingFragmentView()).commit();
+            navigationView.setCheckedItem(R.id.nav_upcoming);
+        }
     }
 
 
@@ -34,5 +47,27 @@ public class NavigationDrawerView extends AppCompatActivity {
             super.onBackPressed();
         }
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_upcoming:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new UpcomingFragmentView()).commit();
+                break;
+            case R.id.nav_history:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HistoryFragmentView()).commit();
+                break;
+            case R.id.nav_sync:
+                Toast.makeText(this, "This is Sync Button!",
+                        Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_logout:
+                Toast.makeText(this, "This is Logout Button!",
+                        Toast.LENGTH_LONG).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
