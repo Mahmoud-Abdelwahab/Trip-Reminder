788b521cd5e821714c0b2f0e174d57259e331bf5
mahmoudfathy01010 <58287190+mahmoudfathy01010@users.noreply.github.com>
2020-03-20 02:12:43 +0200

    Create TripNote.java

---
+++ app/src/main/java/iti/alex/intake40/team9/tripreminder/POJO/TripNote.java
@@ -0,0 +1,17 @@
+package iti.alex.intake40.team9.tripreminder.POJO;
+
+public class TripNote {
+    private String Content;
+
+    public TripNote(String content) {
+        Content = content;
+    }
+
+    public void setContent(String content) {
+        Content = content;
+    }
+
+    public String getContent() {
+        return Content;
+    }
+}
