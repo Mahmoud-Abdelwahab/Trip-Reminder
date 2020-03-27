9d7f87e6e689507fbcbcd30f61a36af29a1fed6a
mahmoudfathy01010 <58287190+mahmoudfathy01010@users.noreply.github.com>
2020-03-28 00:45:18 +0200

    Update Trip.java

--- app/src/main/java/iti/alex/intake40/team9/tripreminder/POJO/Trip.java
+++ app/src/main/java/iti/alex/intake40/team9/tripreminder/POJO/Trip.java
@@ -6,40 +6,40 @@ import java.util.ArrayList;
 
 public class Trip {
     private String tripName;
-    private TripLocation startPoint;
-    private TripLocation endPoint;
-    private TripCalender calender;
-    private ArrayList<TripNote> notes;
+    private String startPoint;
+    private String endPoint;
+    private ArrayList<String> notes;
     private String status;
+    private String date;
+    private String time;
+    private String priorityImage;
 
-    public Trip(String tripName, TripLocation startPoint, TripLocation endPoint,TripCalender calender, ArrayList<TripNote> notes, String status) {
+    public Trip(String tripName, String startPoint, String endPoint, ArrayList<String> notes, String status,String date, String time, String priorityImage) {
         this.tripName = tripName;
         this.startPoint = startPoint;
         this.endPoint = endPoint;
-        this.calender=calender;
         this.notes = notes;
         this.status = status;
+        this.date=date;
+        this.time=time;
+        this.priorityImage=priorityImage;
     }
-    public Trip (String tripName,String tripstatus)
-    {
-        this.tripName=tripName;
-        this.status=tripstatus;
-    }
+
 
     public void setTripName(String tripName) {
         this.tripName = tripName;
     }
 
-    public void setStartPoint(TripLocation startPoint) {
+    public void setStartPoint(String startPoint) {
         this.startPoint = startPoint;
     }
 
-    public void setEndPoint(TripLocation endPoint) {
+    public void setEndPoint(String endPoint) {
         this.endPoint = endPoint;
     }
 
 
-    public void setNotes(ArrayList<TripNote> notes) {
+    public void setNotes(ArrayList<String> notes) {
         this.notes = notes;
     }
 
@@ -47,26 +47,49 @@ public class Trip {
         this.status = status;
     }
 
+    public void setDate(String date) {
+        date = date;
+    }
+
+    public void setTime(String time) {
+        date = time;
+    }
+
+    public void setPriorityImage(String priorityImage) {
+        this.priorityImage = priorityImage;
+    }
 
     public String getTripName() {
         return tripName;
     }
 
-    public TripLocation getStartPoint() {
+    public String getStartPoint() {
         return startPoint;
     }
 
-    public TripLocation getEndPoint() {
+    public String getEndPoint() {
         return endPoint;
     }
 
 
 
-    public ArrayList<TripNote> getNotes() {
+    public ArrayList<String> getNotes() {
         return notes;
     }
 
     public String getStatus() {
         return status;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getPriorityImage() {
+        return priorityImage;
     }
 }
