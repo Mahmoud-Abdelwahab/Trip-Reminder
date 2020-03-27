cb76e1cdcc531e006d53d6c51d2c899c34ee1b4e
mahmoudfathy01010 <58287190+mahmoudfathy01010@users.noreply.github.com>
2020-03-28 00:45:34 +0200

    Update SplashPresenter.java

--- app/src/main/java/iti/alex/intake40/team9/tripreminder/Presenters/SplashPresenter.java
+++ app/src/main/java/iti/alex/intake40/team9/tripreminder/Presenters/SplashPresenter.java
@@ -2,16 +2,10 @@ package iti.alex.intake40.team9.tripreminder.Presenters;
 
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import iti.alex.intake40.team9.tripreminder.Models.FireBaseModel;
-import iti.alex.intake40.team9.tripreminder.POJO.Trip;
-import iti.alex.intake40.team9.tripreminder.POJO.TripDate;
-import iti.alex.intake40.team9.tripreminder.POJO.TripLocation;
-import iti.alex.intake40.team9.tripreminder.POJO.TripTime;
 import iti.alex.intake40.team9.tripreminder.Views.LoginView;
 import iti.alex.intake40.team9.tripreminder.Views.NavigationDrawerView;
 
