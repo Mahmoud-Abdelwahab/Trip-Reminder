93ef4652947263053f1cca27cc093d046bf4f245
mahmoudfathy01010 <58287190+mahmoudfathy01010@users.noreply.github.com>
2020-03-28 00:45:30 +0200

    Update LoginPresenter.java

--- app/src/main/java/iti/alex/intake40/team9/tripreminder/Presenters/LoginPresenter.java
+++ app/src/main/java/iti/alex/intake40/team9/tripreminder/Presenters/LoginPresenter.java
@@ -2,31 +2,16 @@ package iti.alex.intake40.team9.tripreminder.Presenters;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
-import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.util.Arrays;
-import java.util.List;
-
-import iti.alex.intake40.team9.tripreminder.Models.FireBaseModel;
-import iti.alex.intake40.team9.tripreminder.POJO.Trip;
-import iti.alex.intake40.team9.tripreminder.POJO.TripDate;
-import iti.alex.intake40.team9.tripreminder.POJO.TripLocation;
-import iti.alex.intake40.team9.tripreminder.POJO.TripTime;
 import iti.alex.intake40.team9.tripreminder.Views.NavigationDrawerView;
 import iti.alex.intake40.team9.tripreminder.Views.RegisterationView;
 
