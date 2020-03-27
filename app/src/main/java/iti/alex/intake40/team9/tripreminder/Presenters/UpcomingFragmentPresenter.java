2fa9110da6e99942709423755ec6311155a8d954
mahmoudfathy01010 <58287190+mahmoudfathy01010@users.noreply.github.com>
2020-03-28 00:45:37 +0200

    Create UpcomingFragmentPresenter.java

---
+++ app/src/main/java/iti/alex/intake40/team9/tripreminder/Presenters/UpcomingFragmentPresenter.java
@@ -0,0 +1,25 @@
+package iti.alex.intake40.team9.tripreminder.Presenters;
+
+import java.util.ArrayList;
+
+import iti.alex.intake40.team9.tripreminder.Contracts.IUpcomingFragment.UpcomingFragmentContract;
+import iti.alex.intake40.team9.tripreminder.Models.UpcomingFragmentModel;
+import iti.alex.intake40.team9.tripreminder.POJO.Trip;
+import iti.alex.intake40.team9.tripreminder.Views.UpcomingFragmentView;
+
+public class UpcomingFragmentPresenter implements UpcomingFragmentContract.IUpcomingFragmentPresenter {
+    UpcomingFragmentContract.IUpcomingFragmentModel iUpcomingFragmentModel;
+    UpcomingFragmentContract.IUpcomingFragmentView iUpcomingFragmentView;
+    ArrayList<Trip> trips=new ArrayList<Trip>();
+
+    public UpcomingFragmentPresenter(UpcomingFragmentView upcomingFragmentView) {
+        iUpcomingFragmentModel=new UpcomingFragmentModel();
+        iUpcomingFragmentView=upcomingFragmentView;
+    }
+
+    @Override
+    public void getTrips() {
+        trips =iUpcomingFragmentModel.getTripsFromFireBase();
+        iUpcomingFragmentView.onDataReceived(trips);
+    }
+}
