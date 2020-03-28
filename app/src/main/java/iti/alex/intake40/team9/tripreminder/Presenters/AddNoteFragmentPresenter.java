9f2d0a4b70e169a99fdde1d0dfec4ac820ddf3d1
mahmoudfathy01010 <58287190+mahmoudfathy01010@users.noreply.github.com>
2020-03-28 17:30:14 +0200

    Create AddNoteFragmentPresenter.java

---
+++ app/src/main/java/iti/alex/intake40/team9/tripreminder/Presenters/AddNoteFragmentPresenter.java
@@ -0,0 +1,26 @@
+package iti.alex.intake40.team9.tripreminder.Presenters;
+
+import java.util.ArrayList;
+
+import iti.alex.intake40.team9.tripreminder.Contracts.IAddNoteFragment.AddNoteFragmentContract;
+import iti.alex.intake40.team9.tripreminder.Models.AddNoteFragmentModel;
+import iti.alex.intake40.team9.tripreminder.POJO.Trip;
+import iti.alex.intake40.team9.tripreminder.Views.AddNoteFragmentView;
+
+public class AddNoteFragmentPresenter implements AddNoteFragmentContract.IAddNoteFragmentPresenter {
+    AddNoteFragmentContract.IAddNoteFragmentModel iAddNoteFragmentModel;
+    AddNoteFragmentContract.IAddNoteFragmentView iAddNoteFragmentView;
+    ArrayList<Trip> trips=new ArrayList<Trip>();
+
+
+    public AddNoteFragmentPresenter(AddNoteFragmentView addNoteFragmentView) {
+        this.iAddNoteFragmentView=addNoteFragmentView;
+        iAddNoteFragmentModel=new AddNoteFragmentModel();
+    }
+
+    @Override
+    public void getTrips() {
+        trips=iAddNoteFragmentModel.getTripsFromFireBase();
+        iAddNoteFragmentView.onDataReceived(trips);
+    }
+}
