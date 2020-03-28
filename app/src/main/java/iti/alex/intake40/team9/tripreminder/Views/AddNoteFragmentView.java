296d7b7130e75e821eef720d102e729d637d0d44
mahmoudfathy01010 <58287190+mahmoudfathy01010@users.noreply.github.com>
2020-03-28 17:30:17 +0200

    Update AddNoteFragmentView.java

--- app/src/main/java/iti/alex/intake40/team9/tripreminder/Views/AddNoteFragmentView.java
+++ app/src/main/java/iti/alex/intake40/team9/tripreminder/Views/AddNoteFragmentView.java
@@ -14,40 +14,45 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.PopupMenu;
 
 import java.util.ArrayList;
 
-import iti.alex.intake40.team9.tripreminder.Adapters.UpcomingRecyclarViewAdapter;
+import iti.alex.intake40.team9.tripreminder.Contracts.IAddNoteFragment.AddNoteFragmentContract;
 import iti.alex.intake40.team9.tripreminder.POJO.Trip;
+import iti.alex.intake40.team9.tripreminder.Presenters.AddNoteFragmentPresenter;
 import iti.alex.intake40.team9.tripreminder.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AddNoteFragmentView extends Fragment {
+public class AddNoteFragmentView extends Fragment implements AddNoteFragmentContract.IAddNoteFragmentView {
 
-    private ArrayList<String> notes=new ArrayList<String>();
+    private ArrayList<Trip> trips=new ArrayList<Trip>();
     ArrayAdapter adapter;
     EditText note;
+    AddNoteFragmentPresenter addNoteFragmentPresenter;
+    int position;
 
-    public AddNoteFragmentView() {
+    public AddNoteFragmentView(int position) {
+        this.position=position;
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        addNoteFragmentPresenter=new AddNoteFragmentPresenter(this);
+        addNoteFragmentPresenter.getTrips();
         View v= inflater.inflate(R.layout.fragment_add_note_view, container, false);
         final ListView lstview=v.findViewById(R.id.list);
         note =v.findViewById(R.id.Note);
         Button addNote=v.findViewById(R.id.AddNote);
-        adapter = new ArrayAdapter(this.getContext(),android.R.layout.simple_list_item_1,notes);
+        adapter = new ArrayAdapter(this.getContext(),android.R.layout.simple_list_item_1,trips.get(position).getNotes());
         addNote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                notes.add(String.valueOf(AddNoteFragmentView.this.note.getText()));
-              adapter= new ArrayAdapter(AddNoteFragmentView.this.getContext(),android.R.layout.simple_list_item_1,notes);
+                trips.get(position).getNotes().add(String.valueOf(AddNoteFragmentView.this.note.getText()));
+              adapter= new ArrayAdapter(AddNoteFragmentView.this.getContext(),android.R.layout.simple_list_item_1,trips.get(position).getNotes());
               lstview.setAdapter(adapter);
             }
         });
@@ -61,7 +66,7 @@ public class AddNoteFragmentView extends Fragment {
                 adb.setNegativeButton("Cancel", null);
                 adb.setPositiveButton("Ok", new AlertDialog.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        notes.remove(position);
+                        trips.get(position).getNotes().remove(position);
                         adapter.notifyDataSetChanged();
                     }});
                 adb.show();
@@ -72,4 +77,8 @@ public class AddNoteFragmentView extends Fragment {
     }
 
 
+    @Override
+    public void onDataReceived(ArrayList<Trip> trips) {
+        this.trips=trips;
+    }
 }
