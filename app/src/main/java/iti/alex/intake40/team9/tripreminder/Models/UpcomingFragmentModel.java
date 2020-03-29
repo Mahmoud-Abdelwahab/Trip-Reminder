0b14ea2deff5012213fa746da229bbbd5bc3639a
mahmoudfathy01010 <58287190+mahmoudfathy01010@users.noreply.github.com>
2020-03-29 11:59:40 +0200

    Update UpcomingFragmentModel.java

--- app/src/main/java/iti/alex/intake40/team9/tripreminder/Models/UpcomingFragmentModel.java
+++ app/src/main/java/iti/alex/intake40/team9/tripreminder/Models/UpcomingFragmentModel.java
@@ -12,7 +12,7 @@ public class UpcomingFragmentModel implements UpcomingFragmentContract.IUpcoming
         ArrayList<String>notes=new ArrayList<>();
         notes.add("Do not forget to do that");
         notes.add("Do you remember that");
-        Trip trip= new Trip("ITI","KafrEldawar","Alexandria",notes,"Upcoming","12/11/2020","20:25","ay 7aga");
+        Trip trip= new Trip("ITI","KafrEldawar","Alexandria",notes,"Upcoming","12/11/2020","20:25","high_priority_img");
         trips.add(trip);
         trips.add(trip);
         trips.add(trip);
