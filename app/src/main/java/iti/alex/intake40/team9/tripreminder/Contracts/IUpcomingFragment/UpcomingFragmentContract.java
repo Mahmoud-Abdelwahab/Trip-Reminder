e5de8ea7f8d3c3a52038da7ca6f1b158886ab5d9
mahmoudfathy01010 <58287190+mahmoudfathy01010@users.noreply.github.com>
2020-03-30 16:55:35 +0200

    Update UpcomingFragmentContract.java

--- app/src/main/java/iti/alex/intake40/team9/tripreminder/Contracts/IUpcomingFragment/UpcomingFragmentContract.java
+++ app/src/main/java/iti/alex/intake40/team9/tripreminder/Contracts/IUpcomingFragment/UpcomingFragmentContract.java
@@ -7,7 +7,7 @@ import iti.alex.intake40.team9.tripreminder.POJO.Trip;
 
 public interface UpcomingFragmentContract {
     public interface IUpcomingFragmentView{
-        public void onDataReceived(ArrayList<Trip> trips);
+        public void onDataReceived(List<Trip> trips);
     }
     public interface IUpcomingFragmentPresenter{
        public void getTrips();
