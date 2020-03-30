79343e5b4121252ca029f0d166118d2108e4aeaa
mahmoudfathy01010 <58287190+mahmoudfathy01010@users.noreply.github.com>
2020-03-30 16:55:32 +0200

    Update AddNoteFragmentContract.java

--- app/src/main/java/iti/alex/intake40/team9/tripreminder/Contracts/IAddNoteFragment/AddNoteFragmentContract.java
+++ app/src/main/java/iti/alex/intake40/team9/tripreminder/Contracts/IAddNoteFragment/AddNoteFragmentContract.java
@@ -1,17 +1,18 @@
 package iti.alex.intake40.team9.tripreminder.Contracts.IAddNoteFragment;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import iti.alex.intake40.team9.tripreminder.POJO.Trip;
 
 public interface AddNoteFragmentContract {
     public interface IAddNoteFragmentView{
-        public void onDataReceived(ArrayList<Trip> trips);
+        public void onDataReceived(List<Trip> trips);
     }
     public interface IAddNoteFragmentPresenter{
         public void getTrips();
     }
     public interface IAddNoteFragmentModel{
-        public ArrayList<Trip> getTripsFromFireBase();
+        public List<Trip> getTripsFromFireBase();
     }
 }
