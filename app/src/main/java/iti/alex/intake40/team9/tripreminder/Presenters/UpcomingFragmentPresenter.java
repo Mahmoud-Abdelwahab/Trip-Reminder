5320c69743ee0fa6e163041b9fc34a1b94ea3c9f
mahmoudfathy01010 <58287190+mahmoudfathy01010@users.noreply.github.com>
2020-03-30 16:55:48 +0200

    Update UpcomingFragmentPresenter.java

--- app/src/main/java/iti/alex/intake40/team9/tripreminder/Presenters/UpcomingFragmentPresenter.java
+++ app/src/main/java/iti/alex/intake40/team9/tripreminder/Presenters/UpcomingFragmentPresenter.java
@@ -3,6 +3,7 @@ package iti.alex.intake40.team9.tripreminder.Presenters;
 import android.content.Context;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import iti.alex.intake40.team9.tripreminder.Contracts.IUpcomingFragment.UpcomingFragmentContract;
 import iti.alex.intake40.team9.tripreminder.Models.UpcomingFragmentModel;
@@ -12,7 +13,7 @@ import iti.alex.intake40.team9.tripreminder.Views.UpcomingFragmentView;
 public class UpcomingFragmentPresenter implements UpcomingFragmentContract.IUpcomingFragmentPresenter {
     UpcomingFragmentContract.IUpcomingFragmentModel iUpcomingFragmentModel;
     UpcomingFragmentContract.IUpcomingFragmentView iUpcomingFragmentView;
-    ArrayList<Trip> trips=new ArrayList<Trip>();
+    List<Trip> trips=new ArrayList<Trip>();
     Context context;
 
     public UpcomingFragmentPresenter(UpcomingFragmentView upcomingFragmentView , Context context) {
