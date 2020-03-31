532d09fc16b60d268f509086a91dbef9f09a37db
mahmoudfathy01010 <58287190+mahmoudfathy01010@users.noreply.github.com>
2020-03-31 18:09:52 +0200

    Update AddNoteFragmentModel.java

--- app/src/main/java/iti/alex/intake40/team9/tripreminder/Models/AddNoteFragmentModel.java
+++ app/src/main/java/iti/alex/intake40/team9/tripreminder/Models/AddNoteFragmentModel.java
@@ -1,28 +1,49 @@
 package iti.alex.intake40.team9.tripreminder.Models;
 
+import android.content.Context;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import iti.alex.intake40.team9.tripreminder.Contracts.IAddNoteFragment.AddNoteFragmentContract;
 import iti.alex.intake40.team9.tripreminder.POJO.Trip;
+import iti.alex.intake40.team9.tripreminder.POJO.TripConverter;
+import iti.alex.intake40.team9.tripreminder.Room.DbModel;
+import iti.alex.intake40.team9.tripreminder.Room.TripModel;
 
 public class AddNoteFragmentModel implements AddNoteFragmentContract.IAddNoteFragmentModel {
+    Context context;
+    List<TripModel> tripModels = new ArrayList<>();
+    List<Trip> trips= new ArrayList<>();
+
+
+
+    public AddNoteFragmentModel(Context context){
+        this.context = context;
+    }
 
     @Override
     public List<Trip> getTripsFromFireBase() {
-        ArrayList<Trip> trips=new ArrayList<Trip>();
-        ArrayList<String>notes=new ArrayList<>();
-        notes.add("Do not forget to do that");
-        notes.add("Do you remember that");
-        Trip trip= new Trip("ITI","KafrEldawar","Alexandria",notes,"Upcoming","12/11/2020","20:25","ay 7aga");
-        trips.add(trip);
-        trips.add(trip);
-        trips.add(trip);
-        trips.add(trip);
-        trips.add(trip);
-        trips.add(trip);
-        trips.add(trip);
 
-        return trips;
+        DbModel dbModel = new DbModel(context);
+        tripModels=dbModel.getAllTripDb();
+        TripConverter tripConverter = new TripConverter();
+        for(int i=0; i<tripModels.size(); i++)
+        {
+            TripModel tripModel = tripModels.get(i);
+            Trip trip = tripConverter.fromTripModelTotrip(tripModel);
+            trips.add(trip);
+        }
+    return trips;
+
     }
+    public void addNotestoDataBase(Trip trip){
+        Trip mytrip = trip;
+        TripConverter tripConverter = new TripConverter();
+        DbModel dbModel = new DbModel(context);
+        dbModel.updateTripDb(tripConverter.fromTripToTripModel(trip));
+    }
+
+
+
 }
