e0951a1b93556805bc7192b421739f5f6c4a872e
mahmoudfathy01010 <58287190+mahmoudfathy01010@users.noreply.github.com>
2020-04-02 02:01:40 +0200

    Update NavigationDrawerView.java

--- app/src/main/java/iti/alex/intake40/team9/tripreminder/Views/NavigationDrawerView.java
+++ app/src/main/java/iti/alex/intake40/team9/tripreminder/Views/NavigationDrawerView.java
@@ -8,48 +8,63 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.text.Layout;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import iti.alex.intake40.team9.tripreminder.Models.FireBaseModel;
 import iti.alex.intake40.team9.tripreminder.Models.FloatingItem;
+import iti.alex.intake40.team9.tripreminder.POJO.Trip;
+import iti.alex.intake40.team9.tripreminder.POJO.TripConverter;
+import iti.alex.intake40.team9.tripreminder.Presenters.UpcomingFragmentPresenter;
 import iti.alex.intake40.team9.tripreminder.R;
+import iti.alex.intake40.team9.tripreminder.Room.DbModel;
+import iti.alex.intake40.team9.tripreminder.Room.TripModel;
 
 public class NavigationDrawerView extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private static final int CODE_DRAW_OVER_OTHER_APP_PERMISSION = 2084;
     DrawerLayout drawer;
     View navHeader;
     TextView email;
+    ImageView profilePicture;
     private UpcomingFragmentView upcomingFragmentView;
 
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_main);
-        navHeader=getLayoutInflater().inflate(R.layout.nav_header,null);
+       View headerView;
+//        navHeader=getLayoutInflater().inflate(R.layout.nav_header,null);
        Toolbar toolbar=findViewById(R.id.toolbar);
        setSupportActionBar(toolbar);
         drawer = findViewById(R.id.drawer_layout);
-
         NavigationView navigationView=findViewById(R.id.nav_view);
-        email=navHeader.findViewById(R.id.useremail);
+        headerView = navigationView.getHeaderView(0);
+        email=headerView.findViewById(R.id.uemail);
+        profilePicture=headerView.findViewById(R.id.profile_image);
         navigationView.setNavigationItemSelectedListener(this);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         Log.i("ay7aga", FireBaseModel.sharedInstance.getUserEmail());
         email.setText(FireBaseModel.sharedInstance.getUserEmail());
+        Bitmap bitmap = FireBaseModel.sharedInstance.getProfileImage();
+        profilePicture.setImageBitmap(FireBaseModel.sharedInstance.getProfileImage());
         toggle.syncState();
 
         if(savedInstanceState==null)
         {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new UpcomingFragmentView()).commit();
             navigationView.setCheckedItem(R.id.nav_upcoming);
+
         }
     }
 
@@ -75,8 +90,18 @@ public class NavigationDrawerView extends AppCompatActivity implements Navigatio
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HistoryFragmentView()).commit();
                 break;
             case R.id.nav_sync:
-                Toast.makeText(this, "This is Sync Button!",
-                        Toast.LENGTH_LONG).show();
+
+                List<Trip> trips= new ArrayList<>();
+                trips =FireBaseModel.sharedInstance.getTrips();
+                for(int i=0; i<trips.size();i++)
+                {
+                    FireBaseModel.sharedInstance.deleteTrip(trips.get(i));
+                }
+                trips=getTripsFromRoom();
+                for(int i=0; i<trips.size();i++)
+                {
+                    FireBaseModel.sharedInstance.addTrip(trips.get(i));
+                }
                 break;
             case R.id.nav_logout:
                 FireBaseModel.sharedInstance.signOut();
@@ -104,6 +129,21 @@ public class NavigationDrawerView extends AppCompatActivity implements Navigatio
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
+    }
+    public List<Trip> getTripsFromRoom() {
+        List<TripModel> tripModels = new ArrayList<>();
+        List<Trip> trips= new ArrayList<>();
+
+        DbModel dbModel = new DbModel(this);
+        tripModels=dbModel.getAllTripDb();
+        for(int i=0 ; i<tripModels.size();i++)
+        {
+            Trip trip=new Trip();
+            TripConverter tripConverter = new TripConverter();
+            trips.add(tripConverter.fromTripModelTotrip(tripModels.get(i)));
+        }
+
+        return trips;
     }
 
 
